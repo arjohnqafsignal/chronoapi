85eaab7abdb39d7d7b14ef23d4a0f327ee795f99
arjohn <arjohnqafsignal@gmail.com>
2020-06-24 12:23:47 +0300

    update register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,8 +50,7 @@ class RegisterController extends Controller
             'first_name' => $data['first_name'],
             'last_name' => $data['last_name'],
             'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-            'ip_address' => $data['ip_address']
+            'password' => Hash::make($data['password'])
         ]);
 
         if($user){
